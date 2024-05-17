211e43a265216f6887c414b1ab0bdd958d51057b
TamaoMiura <miuratamao.d@gmail.com>
2024-05-17 11:55:32 +0900

    showl countries where i visited

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "12345", //change correct password!
+  port: 5432,
+})
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  console.log(result.rows);
+  result.rows.forEach((country) => {
+    console.log(country);
+    countries.push(country.country_code);
+  })
+  res.render("index.ejs", {
+    total: countries.length,
+    countries: countries
+  });
+  db.end();
 });
 
 app.listen(port, () => {
