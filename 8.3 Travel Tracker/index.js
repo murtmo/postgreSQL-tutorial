6cce965d8a2cc57ee21e5bc8794a0cb2706e289f
TamaoMiura <miuratamao.d@gmail.com>
2024-05-17 14:09:50 +0900

    insert data to database

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -22,16 +22,35 @@ app.get("/", async (req, res) => {
 
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
-  console.log(result.rows);
+  // console.log(result.rows);
   result.rows.forEach((country) => {
-    console.log(country);
+    // console.log(country);
     countries.push(country.country_code);
   })
   res.render("index.ejs", {
     total: countries.length,
     countries: countries
   });
-  db.end();
+  // db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_id FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_id;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
