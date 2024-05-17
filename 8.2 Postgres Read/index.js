b205115140013aaeb0981b4eb8d5c5236f58ee68
TamaoMiura <miuratamao.d@gmail.com>
2024-05-17 10:45:55 +0900

    create world flags quiz

--- 8.2 Postgres Read/index.js
+++ 8.2 Postgres Read/index.js
@@ -1,8 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "TamaoM1103", //change correct password!
+  port: 5432,
+})
+
+db.connect();
+
+let quiz = [];
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
